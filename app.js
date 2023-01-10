cfccb845d51bc48ddd81dfadb0cf88563b68ae4e
iiiosidosdd <117013595+iiiosidosdd@users.noreply.github.com>
2023-01-10 08:11:57 +0000

    Update app.js

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.use(
 /* keepalive  begin */
 function keepalive() {
   // 1.请求主页，保持唤醒
-  let render_app_url = "https://nodejs-express-test-7lve.onrender.com"
+  let render_app_url = "https://two000000-rend-2000000.onrender.com","https://twotwo000000-rend-2000000.onrender.com"
   request(render_app_url, function (error, response, body) {
     if (!error) {
       console.log("主页发包成功！");
